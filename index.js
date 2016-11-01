6cd73b6794b1a1041daf7661ff37cead9be7ba95
Tyler Long <tyler4long@gmail.com>
2016-11-01 23:18:50 +0800

    Replace jQuery with React

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-const $ = require('jquery');
 const mdc = require('markdown-core/markdown-core-node');
 require('./index.css');
+const React = require('react');
+const ReactDOM = require('react-dom');
 
 
-$(function(){
-  $('#markdown-textarea').on('input propertychange paste', function() {
-    const markdown = $(this).val();
-    const html = mdc.render(markdown);
-    $('.markdown-body').html(html);
-  });
-});
+class MarkdownEditor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { markdown: '' };
+  }
+  render() {
+    return (
+      <div>
+        <textarea ref="markdown" id="markdown-textarea" value={this.state.markdown} onChange={() => this.setState({ markdown: this.refs.markdown.value })}></textarea>
+        <div className="markdown-body" dangerouslySetInnerHTML={{ __html: mdc.render(this.state.markdown) }}></div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<MarkdownEditor />, document.getElementById('root'));
